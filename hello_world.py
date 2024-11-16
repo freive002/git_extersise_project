ef6aa0a6d7572e2742c3d9a2ddbf8368ab29d0d6
freive002 <freive00933@stud.noroff.no>
2024-11-16 12:32:21 +0100

    Firt Commit

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+morning_message = "Good morning "
+name = "Fredrik"
+clockhour = 10
+clockminute = 42
+clock_text = ", the clock is: "
+
+print(f"{morning_message}{name}{clock_text}{clockhour}:{clockminute}")
